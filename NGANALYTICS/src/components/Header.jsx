6ec4b8592fbd9dc31b085e0f2dd209919cd1cf37
SOCSCFUTA <connnectreads@gmail.com>
2025-05-23 20:18:24 +0100

    Add files via upload

---
+++ NGANALYTICS/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+
+const Header = () => {
+    const[mobileMenuOpen,setMobileMenuOpen]=useState(false)
+  return (
+    <header className='bg-white border-b border-gray-100 sticky'>
+        <div>
+            <div>
+
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
